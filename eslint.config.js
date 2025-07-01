10eca86a4ac5c34a176282acf54abe60670624ba
Take the last chance <2238304980@qq.com>
2025-07-01 12:51:33 +0800

    build test

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,13 @@ import { defineFlatConfig } from "eslint-define-config";
 import js from "@eslint/js";
 import vuePlugin from "eslint-plugin-vue";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import markdownPlugin from "eslint-plugin-markdown";
 import globals from "globals";
 
 export default defineFlatConfig([
   js.configs.recommended,
   {
-    // TypeScript 配置
+    // TypeScript 和 Vue 配置
     plugins: {
       "@typescript-eslint": tsPlugin,
       vue: vuePlugin,
@@ -27,10 +28,22 @@ export default defineFlatConfig([
       },
     },
     rules: {
-      // 自定义规则
       "vue/multi-word-component-names": "off",
       "@typescript-eslint/no-unused-vars": "warn",
     },
+  },
+  {
+    // Markdown 文件特殊处理
+    files: ["**/*.md"],
+    plugins: {
+      markdown: markdownPlugin
+    },
+    processor: "markdown/markdown",
+    rules: {
+      "no-undef": "off",
+      "no-unused-vars": "off",
+      "no-console": "off"
+    }
   },
   {
     ignores: [
@@ -38,7 +51,8 @@ export default defineFlatConfig([
       "**/dist/",
       "**/coverage/",
       ".vitepress/cache/",
-      "**/*.json"
+      "**/*.json",
+      "**/docs/.vitepress/"
     ],
   },
 ]);
