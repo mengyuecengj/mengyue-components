7e5a754635aa1c7aa4ad6767f30c53e2be4983bb
mengyuecengj <mengyuecengj@users.noreply.github.com>
2025-08-06 03:30:09 +0000

    deploy: 5b132e73a9052ad3b12e6ad57ef4e041011c65df

---
+++ assets/markdown-examples.md.CqrMs_-k.js
@@ -0,0 +1,36 @@
+import{_ as a,c as n,o as p,af as e}from"./chunks/framework.BlLknbtB.js";const m=JSON.parse('{"title":"Markdown Extension Examples","description":"","frontmatter":{},"headers":[],"relativePath":"markdown-examples.md","filePath":"markdown-examples.md"}'),l={name:"markdown-examples.md"};function i(t,s,o,r,c,d){return p(),n("div",null,s[0]||(s[0]=[e(`<h1 id="markdown-extension-examples" tabindex="-1">Markdown Extension Examples <a class="header-anchor" href="#markdown-extension-examples" aria-label="Permalink to &quot;Markdown Extension Examples&quot;">​</a></h1><p>This page demonstrates some of the built-in markdown extensions provided by VitePress.</p><h2 id="syntax-highlighting" tabindex="-1">Syntax Highlighting <a class="header-anchor" href="#syntax-highlighting" aria-label="Permalink to &quot;Syntax Highlighting&quot;">​</a></h2><p>VitePress provides Syntax Highlighting powered by <a href="https://github.com/shikijs/shiki" target="_blank" rel="noreferrer">Shiki</a>, with additional features like line-highlighting:</p><p><strong>Input</strong></p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki nord" style="color:#d8dee9ff;" tabindex="0"><code><span class="line"><span>\`\`\`js{4}</span></span>
+<span class="line"><span>export default {</span></span>
+<span class="line"><span>  data () {</span></span>
+<span class="line"><span>    return {</span></span>
+<span class="line"><span>      msg: &#39;Highlighted!&#39;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>  }</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span>\`\`\`</span></span>
+<span class="line"><span></span></span></code></pre></div><p><strong>Output</strong></p><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki nord" style="color:#d8dee9ff;" tabindex="0"><code><span class="line"><span style="color:#81A1C1;">export</span><span style="color:#81A1C1;"> default</span><span style="color:#ECEFF4;"> {</span></span>
+<span class="line"><span style="color:#88C0D0;">  data</span><span style="color:#ECEFF4;"> ()</span><span style="color:#ECEFF4;"> {</span></span>
+<span class="line"><span style="color:#81A1C1;">    return</span><span style="color:#ECEFF4;"> {</span></span>
+<span class="line"><span style="color:#88C0D0;">      msg</span><span style="color:#ECEFF4;">:</span><span style="color:#ECEFF4;"> &#39;</span><span style="color:#A3BE8C;">Highlighted!</span><span style="color:#ECEFF4;">&#39;</span></span>
+<span class="line"><span style="color:#ECEFF4;">    }</span></span>
+<span class="line"><span style="color:#ECEFF4;">  }</span></span>
+<span class="line"><span style="color:#ECEFF4;">}</span></span>
+<span class="line"></span></code></pre></div><h2 id="custom-containers" tabindex="-1">Custom Containers <a class="header-anchor" href="#custom-containers" aria-label="Permalink to &quot;Custom Containers&quot;">​</a></h2><p><strong>Input</strong></p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki nord" style="color:#d8dee9ff;" tabindex="0"><code><span class="line"><span>::: info</span></span>
+<span class="line"><span>This is an info box.</span></span>
+<span class="line"><span>:::</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>::: tip</span></span>
+<span class="line"><span>This is a tip.</span></span>
+<span class="line"><span>:::</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>::: warning</span></span>
+<span class="line"><span>This is a warning.</span></span>
+<span class="line"><span>:::</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>::: danger</span></span>
+<span class="line"><span>This is a dangerous warning.</span></span>
+<span class="line"><span>:::</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>::: details</span></span>
+<span class="line"><span>This is a details block.</span></span>
+<span class="line"><span>:::</span></span>
+<span class="line"><span></span></span></code></pre></div><p><strong>Output</strong></p><div class="info custom-block"><p class="custom-block-title">INFO</p><p>This is an info box.</p></div><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>This is a tip.</p></div><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>This is a warning.</p></div><div class="danger custom-block"><p class="custom-block-title">DANGER</p><p>This is a dangerous warning.</p></div><details class="details custom-block"><summary>Details</summary><p>This is a details block.</p></details><h2 id="more" tabindex="-1">More <a class="header-anchor" href="#more" aria-label="Permalink to &quot;More&quot;">​</a></h2><p>Check out the documentation for the <a href="https://vitepress.dev/guide/markdown" target="_blank" rel="noreferrer">full list of markdown extensions</a>.</p>`,19)]))}const g=a(l,[["render",i]]);export{m as __pageData,g as default};
